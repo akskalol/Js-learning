a3ef0a832019879b1daaf122dc6e6cb075aaf9cb
aftab shekh <114379394+akskalol@users.noreply.github.com>
2023-09-23 03:26:12 +0000

    Update

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,13 @@
 const accontID=144553
-let acoountEmail="bayareaheath@mca.com"
+let accountEmail="bayareaheath@mca.com"
 var accountPassword="12345"
 accountCity="Freakmont"
 
-accountId=2
+// accountId=2
+accountEmail="bach@bach.com"
+accountPassword="abcd"
+accountCity="hawkings"
 
 console.log(accountId);
+
+// console.table([accountId,acoountEmail,accountPassword,accountCity])
