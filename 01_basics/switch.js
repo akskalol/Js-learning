7dd52e6341e4ecf09c8392d4a81b09678afd292c
aftab shekh <114379394+akskalol@users.noreply.github.com>
2023-10-02 13:03:00 +0000

    control statement

---
+++ 01_basics/switch.js
@@ -0,0 +1,23 @@
+const month =6
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Fab");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
+
+//falsy value
+
+//false,0,-0,BigInt 0n,"",null,undefined,NaN
