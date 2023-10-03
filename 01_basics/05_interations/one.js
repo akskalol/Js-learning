f647d66657a5f4319de51f76f7245e6377a51f3b
aftab shekh <114379394+akskalol@users.noreply.github.com>
2023-10-03 23:53:43 +0000

    inner loop

--- 01_basics/05_interations/one.js
+++ 01_basics/05_interations/one.js
@@ -1,7 +1,18 @@
 //for loop
- for (let i = 0; i < 50; i++) {
+ for (let i = 0; i <=10; i++) {
    let element = i;
-   console.log(element);
+   if(element==5) {
+      //console.log("5 is best number");
+   }
+   //console.log(element);
    
  }
- 
+  
+ for (let i = 0; i <= 10; i++) {
+   console.log(`outer loop value:${i}`);
+  for (let j = 0; j <=10; j++) {
+  console.log(`inner loop value:${j} and inner loop ${i}`);
+   
+  }
+   
+ }
